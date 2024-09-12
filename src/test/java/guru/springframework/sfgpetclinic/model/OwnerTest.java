80dd946cd41377fea09e6c52bb383ffb2035e798
Alaa Zamel <alaa.zamel23@gmail.com>
2024-09-12 21:23:13 +0300

    skipping a test

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        owner.setCity("Key West");
+        owner.setTelephone("1231231234");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Joe", owner.getFirstName(), "First Name is not matching"),
+                        () -> assertEquals("Buck", owner.getLastName(), "Last Name is not matching")
+                ),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Key West", owner.getCity(), "City is not matching"),
+                        () -> assertEquals("1231231234", owner.getTelephone(), "Telephone is not matching")
+                )
+        );
+
+    }
+}
---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,45 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class OwnerSDJpaServiceTest {
+
+    OwnerSDJpaService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new OwnerSDJpaService(null, null, null);
+    }
+
+    @Test
+    @Disabled("Disabled until we learn mocking")
+    void findByLastName() {
+        service.findByLastName("Zamel");
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
