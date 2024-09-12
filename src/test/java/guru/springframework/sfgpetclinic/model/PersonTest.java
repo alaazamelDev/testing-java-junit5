2c2f703af8f6ad997884dcf926f9c2236f612357
Alaa Zamel <alaa.zamel23@gmail.com>
2024-09-12 16:41:14 +0300

    person test

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonTest {
+
+    @Test
+    void groupedAssertions() {
+        // given
+        Person person = new Person(1L, "Alaa", "Zamel");
+
+        // then
+        assertAll("Test Props Set",
+                () -> assertEquals("Alaa", person.getFirstName(), "First Name is not matching"),
+                () -> assertEquals("Zamel", person.getLastName(), "Last Name is not matching")
+        );
+    }
+}
